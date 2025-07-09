331449b47364144a467a62ba83d4a9a4f2ee92c3
taisuke91 <itoutaisuke20030430@gmail.com>
2025-07-09 14:20:32 +0900

    layoutについて

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-slate-900 text-white p-4 text-center">
+            <p>Welcome to Next.js 15 Crash Course</p>
+        </header>
         {children}
+        <footer className="bg-slate-900 text-white p-4 text-center">
+            Code evolution
+        </footer>
       </body>
     </html>
   );
